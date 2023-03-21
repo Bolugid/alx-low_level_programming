c133ca4f4b7fb1dc5b275c5dfe2fff20995fabf2
Bolugid <bolu0439@gmail.com>
2023-03-21 13:19:39 +0100

    My C variable

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/*
+ * main - Prints all possible combinations of two different digits,
+ *	in ascending order, separated by a comma followed by a space.
+ * Description: 'print diff combination with putchar'
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 0; n < 9; n++)
+	{
+		for (m = n + 1; m < 10; m++)
+		{
+			putchar((n % 10) + '0');
+			putchar((m % 10) + '0');
+
+			if (n == 8 && m == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
